91a714c18d33fe19c144d6bab5db2fc76e61ecbc
Dreycey <albindreycey@gmail.com>
2021-09-17 21:28:47 -0600

    adding the temperature plotiing script.

---
+++ scripts/temperature_plotter.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+"""
+DESCRIPTION:
+    This script is ussed to plot figure from the output
+    of the temperature output.
+TO DO:
+    1. Plot the temperature of each thermometer over time.
+    2. Print the error for all 3 thermometers for different
+       temperature ranges. For this, the config file should 
+       also be used to ensure that the proper tracking followed.
+    3. A plot of the difference between the magnetic themometer
+       and the plastic thermometer over different temperature 
+       values specified in the confiiguration file.
+    4. Derivative of temperature increase over time, and 
+       the box-whiskers at different temperature values. 
+    5. eventually - if new controller added, compare average error
+       over time with the average error of the new controller. 
+"""
